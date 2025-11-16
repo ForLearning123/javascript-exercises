27a0898e94d0092affd817860db00da69c5ece3c
ForLearning123 <237149068+ForLearning123@users.noreply.github.com>
2025-11-16 07:24:39 -0800

    solve palindrome exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let ordered = str.toUpperCase();
+    ordered = ordered.split("").filter(
+      (x) => x != "." && x != " " && x != "!" && x != ","
+    );
+  
+    let reversed = "";
+    for (let i = ordered.length -1; i >= 0; i--){
+      reversed += ordered[i]
+    }
+
+    ordered = ordered.join("");
+    return ordered === reversed;
 
 };
-
 // Do not edit below this line
 module.exports = palindromes;
