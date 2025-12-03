7250d25c399967dc8657d0ab26832e8b6c04e2e3
ForLearning123 <237149068+ForLearning123@users.noreply.github.com>
2025-12-03 00:10:22 -0800

    solve findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const currentDate = new Date();
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        if (element.yearOfDeath === undefined){
+            element.yearOfDeath =  currentDate.getFullYear();
+        };
+        
+    }
 
+    array.sort((a, b) => {
+        if (a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth){
+            return 1;
+        } else {
+            return -1;
+        };
+    })
+    return array.at(-1);
 };
 
 // Do not edit below this line
